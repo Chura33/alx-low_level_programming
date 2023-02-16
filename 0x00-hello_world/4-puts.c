56ad6c70a6b16eea043453e757d42147b943f711
Chura33 <churabrady@gmail.com>
2023-02-16 11:11:51 -0800

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
-#include <stdio.h>
+ #include <stdio.h>
+/**
+  * main-program entry point
+  *
+  *Return: returns 0
+  */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
-return (0);
-};	
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
