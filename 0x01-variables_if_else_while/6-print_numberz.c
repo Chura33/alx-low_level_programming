aac9f94dba35bab509e1216abe09d6f96e9736e6
Chura33 <churabrady@gmail.com>
2023-02-17 11:41:05 -0800

    add numbers with char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main- main entry of the program
+*
+*Return: return 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = '0' ; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
