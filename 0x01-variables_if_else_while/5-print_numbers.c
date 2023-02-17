971fa6195f8bb1f073a7abab12145edaaaf0d8a1
Chura33 <churabrady@gmail.com>
2023-02-17 10:13:10 -0800

    zeroto9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main- main entry of the program
+*
+*Return: return 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/:! betty 5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main- main entry of the program
+*
+*Return: return 0
+*/
+int main(void)
+{
+	for (i = 0 ; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
