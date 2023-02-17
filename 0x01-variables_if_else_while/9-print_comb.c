883f85bcc996133b07f0a454626422d7e5a609ac
Chura33 <churabrady@gmail.com>
2023-02-17 13:30:57 -0800

    patience and a new line

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,12 @@ int main(void)
 {
 	int i;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		putchar(i);
-		if (i == '9')
+		putchar(i + '0');
+		if (i == 9)
 		{
+			putchar('\n');
 			exit(0);
 		}
 
