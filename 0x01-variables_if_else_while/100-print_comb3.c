73f392b1748f06829c194e15690c4771feae9d85
Chura33 <churabrady@gmail.com>
2023-02-17 14:40:31 -0800

    another method

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,12 +18,14 @@ int main(void)
 			{
 				putchar(i + '0');
 				putchar(j + '0');
-				if (i == 8 && j == 9)
-					putchar('\n');
-				else
+				if (i + j != 17)
 				{
 					putchar(',');
 					putchar(' ');
+				}
+				else
+				{
+					putchar('\n');
 				}
 			}
 		}
