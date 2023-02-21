cba31f85be635875c6687cba3d4722dd5f7515dc
Chura33 <churabrady@gmail.com>
2023-02-21 13:25:31 -0800

    alphabet stuff

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_alphabet - this prints all the alphabet
+* Return: nothing
+*/
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 97; a < 123; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
