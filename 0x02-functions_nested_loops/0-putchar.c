6edec805b1bce7216551c498e5cacffb25931cab
Chura33 <churabrady@gmail.com>
2023-02-21 11:02:24 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+/**
+* main - main entry point
+* Return: returns 0
+*/
+int main(void)
+{
+	char name_to_print[] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < (int)strlen(name_to_print); i++)
+	{
+		putchar(name_to_print[i]);
+	}
+	putchar('\n');
+	return (0);
+}
