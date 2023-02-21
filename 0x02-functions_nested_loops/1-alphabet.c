5105a9508256fbb52c1507b74bcfb1d6a74d8738
Chura33 <churabrady@gmail.com>
2023-02-21 11:46:17 -0800

    alphabet stuff

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* main- main entry point
+* Return: returns 0
+*/
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+/**
+* print_alphabet- a function that prints all the alphabets
+*/
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 97; a < 123; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+}
