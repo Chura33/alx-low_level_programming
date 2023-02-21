1c3cd1feda9ef2223c2e38f58b8996bde4ceed68
Chura33 <churabrady@gmail.com>
2023-02-21 13:04:25 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
 * main - main entry point
+* _putchar - returns the string _putchar
 * Return: returns 0
 */
 int main(void)
@@ -11,8 +12,8 @@ int main(void)
 
 	for (i = 0; i < (int)strlen(name_to_print); i++)
 	{
-		putchar(name_to_print[i]);
+		_putchar(name_to_print[i]);
 	}
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,15 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 #include <string.h>
-/**
-* print_alphabet- a function that prints all the alphabets
-*/
-void print_alphabet(void)
-{
-	char a;
-
-	for (a = 97; a < 123; a++)
-	{
-		putchar(a);
-	}
-	putchar('\n');
-}
+int _putchar(char c);
+#endif
