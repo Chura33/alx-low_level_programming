ec8e009791c6d078f8372e92ddd2a42cad22242e
Chura33 <churabrady@gmail.com>
2023-02-23 04:50:22 -0800

    leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,9 +12,9 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if (((n % 4 == 0) && (n % 400 == 0)) || (n % 4 == 0)  && (n % 100 != 0) && (n % 400 != 0))
+	if (((year % 4 == 0) && (year % 400 == 0)) || ((year % 4 == 0)  && (year % 100 != 0) && (year % 400 != 0)))
 	{
-		if (month >= 2 && day >= 60)
+		if (month > 2 && day >= 60)
 		{
 		    day++;
 		}
