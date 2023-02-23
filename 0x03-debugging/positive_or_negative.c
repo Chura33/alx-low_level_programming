725359ab01ee5b485b39662eb3e3b7077f83b963
Chura33 <churabrady@gmail.com>
2023-02-23 08:25:50 -0800

    zero is zero

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* positive_or_negative - prints out whether a number is positive or negative
+* @i - a function parameter
+* Return: returns nothing
+*/
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else
+	{
+		printf("%d is negative\n", i);
+	}
+}
