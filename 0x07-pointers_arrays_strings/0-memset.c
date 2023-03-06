83c0ac548ad8336bf00e0cdd503a6a3c0e3dd999
Chura33 <churabrady@gmail.com>
2023-03-06 09:36:03 -0800

    0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * _memset(char *s, char b, unsigned int n)- This is the main entry point of the function
+ * _memset(char *s, char b, unsigned int n)- This
+ * is the main entry point of the function
  * @s: This is the pointer to the character array
  * @b: This is the parameter that defines the character to be filled
  * @n: This is the space in bytes that is taken in the memory
