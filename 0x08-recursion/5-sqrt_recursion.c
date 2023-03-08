89f8b16c9fa314117ae7fb7aee2ee2ad0fa1d878
Chura33 <churabrady@gmail.com>
2023-03-08 11:49:05 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 #include "main.h"
+
+int checksqrt(int n, int i);
+
 /**
-* int _sqrt_recursion(int n)- returns the square root of a number
+* _sqrt_recursion(int n)- returns the square root of a number
 * or returns -1 if the squareroot is not an integer
 * @n: this is the number param
 * Return: returns an int
-*/
-
-int checksqrt(int n, int i); 
-/**
-* checksqrt(int n, int i)- checks for the square root of number
-* @n: the number to be operated on
-* @i: the counter
 */
 
 int _sqrt_recursion(int n)
@@ -20,13 +16,20 @@ int _sqrt_recursion(int n)
 	{
 		return (-1);
 	}
-	return (checksqrt(n,0));
+	return (checksqrt(n, 0));
 }
+
+/**
+* checksqrt(int n, int i)- checks for the square root of number
+* @n: the number to be operated on
+* @i: the counter
+* Return: returns an integer
+*/
 
 int checksqrt(int n, int i)
 {
 	if (i * i < n)
-		return (checksqrt(n, i+1));
+		return (checksqrt(n, i + 1));
 	else if (i * i == n)
 		return (i);
 	return (-1);
