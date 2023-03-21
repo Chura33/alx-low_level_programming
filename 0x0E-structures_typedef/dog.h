4b20261960d2de02b9700a65fc077401139e288e
Chura33 <churabrady@gmail.com>
2023-03-21 02:28:49 -0700

    compile poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* struct dog - this is a struct that defines a dog
+* @name: name of the dog
+* @age: The dog's age
+* @owner: The owner of the dog
+*/
+
+struct dog
+{
+	char* name;
+	float age;
+	char* owner;
+};
+
+
