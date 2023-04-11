f55537b02b141f1be345b437bdd789e28cd548d6
Chura33 <churabrady@gmail.com>
2023-04-11 14:47:56 -0700

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_binary - Prints the binary of a number
+*@n: the number to conver to binary
+*/
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		prnt_binary(n >> 1);
+	putchar((n & 1) + '0');
+}
