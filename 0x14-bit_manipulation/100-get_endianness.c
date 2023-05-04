84960caaa6384d270a78e8b106326c2ad62b577f
Your Name <you@example.com>
2023-05-04 11:14:01 -0700

    compiled getendianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ * get_endianness - tells whether a machine is a little or big endian
+ * Return: returns 0 for big endia, 1 for little endian
+ */
+
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *e = (char *) &i;
+
+	return (*e);
+}
