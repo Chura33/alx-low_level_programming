3fcfa0057163a3c55a00f5e246e071bb9660fe74
Your Name <you@example.com>
2023-05-04 11:06:03 -0700

    compiled 4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* clear_bit - this function clears the value of a bit at a given point
+* @n: the number to work on
+* @index: the index of the number
+* Return: returns 1 on success and -1 on failure
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	int cover;
+
+	if (index > 63)
+	{
+	return (-1);
+	}
+
+	cover = 1 << index;
+
+	*n = (*n & ~cover);
+	return (1);
+}
