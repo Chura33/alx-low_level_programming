5e2fabda1947d33949eab37317b016fcb0c5809d
Chura33 <churabrady@gmail.com>
2023-05-27 03:06:25 -0700

    latest solution

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 			length++;
 	}
 
-	o = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+	o = open(filename, O_CREAT |O_RDWR |O_TRUNC, 0600);
 	w = write(o, text_content, length);
 
 	if (o == -1 || w == -1)
