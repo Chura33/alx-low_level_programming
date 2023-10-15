79e0e788a5bb2cf0c72529c3facdd2d44f28ca6d
Chura33 <churabrady@gmail.com>
2023-10-15 06:34:26 -0700

    solved task 7

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -14,9 +14,9 @@ int jump_search(int *array, size_t size, int value)
 {
 	int i = 0, temp = 0, count = 0, increment = (int)(floor(sqrt(size)));
 
-	if (!array || size == 0)
+	if (!array)
 		return (-1);
-	while (array[i] < value && i < (int)size - 1)
+	while (array[i] < value && i < (int)size)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		temp = i;
